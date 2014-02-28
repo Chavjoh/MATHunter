1f6acf3581a4abd6a9ef62367afcce93e038a74b
Chavjoh <j.chavaillaz@gmail.com>
2014-02-28 16:09:37 +0100

    Add image column for tutorials

--- MATHunter/db/schema.rb
+++ MATHunter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222143006) do
+ActiveRecord::Schema.define(version: 20140228150813) do
 
   create_table "chapters", force: true do |t|
     t.integer  "tutorial_id"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140222143006) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   add_index "tutorials", ["user_id"], name: "index_tutorials_on_user_id", using: :btree
