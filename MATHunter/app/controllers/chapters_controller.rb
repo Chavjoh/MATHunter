ad1d8eeb4732412adce2d40ed4985af9dc48d1a3
Dany Jupille <dany.jupille@he-arc.ch>
2014-02-28 14:41:50 +0100

    Continued tutorials and chapters implementation. - Implemented chapters management - Modified routes for chapteres accessibility

--- MATHunter/app/controllers/chapters_controller.rb
+++ MATHunter/app/controllers/chapters_controller.rb
@@ -1,26 +1,51 @@
 class ChaptersController < ApplicationController
   
   def index
-    
+    @chapters = Chapter.all
   end
   
   def show
-    
+    @chapter = Chapter.find(params[:id])
   end
   
   def new
-    
+    @chapter = Chapter.new
+  end
+  
+  def edit
+    @chapter = Chapter.find(params[:id])
   end
   
   def create
+    @chapter = Chapter.new(get_params)
     
+    if @chapter.save
+      redirect_to @chapter
+    else
+      render 'new'
+    end
   end
   
   def update
+    @chapter = Chapter.find(params[:id])
     
+    if @chapter.update(get_params)
+      redirect_to @chapter
+    else
+      render 'edit'
+    end
   end
   
   def destroy
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
     
+    redirect_to chapters_path
+  end
+  
+  private
+  
+  def get_params
+    params[:chapter].permit(:title, :difficulty, :description)
   end
 end
