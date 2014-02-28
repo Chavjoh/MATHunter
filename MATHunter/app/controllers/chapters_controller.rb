5f85f60e71af7e4bcb516141bfa2b79320f58ad0
Dany Jupille <dany.jupille@he-arc.ch>
2014-02-28 14:16:24 +0100

    Started tutorials and chapters implementation. - Added tutorials controller and views - Added chapters controller and views - Implemented tutorials management - Modified routes for tutorials accessibility

---
+++ MATHunter/app/controllers/chapters_controller.rb
@@ -0,0 +1,26 @@
+class ChaptersController < ApplicationController
+  
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+end
