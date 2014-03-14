52c9053993ed0f0258fb88c563b0c665938792f5
Chavjoh <j.chavaillaz@gmail.com>
2014-03-14 15:43:29 +0100

    Finish comment system.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
   def create
+    # Create comment in chapter
     @chapter = Chapter.find(params[:chapter_id])
-    @comment = @chapter.comments.create(params[:comment].permit(:message))
-    redirect_to chapter_path(@chapter)
+    @comment = @chapter.comments.create(params[:comment].permit(:content))
+    
+    # Add user ID from session
+    @comment.user_id = session[:user_id]
+    @comment.save
+    
+    redirect_to tutorial_chapter_path({:tutorial_id => @chapter.tutorial.id, :id => params[:chapter_id]})
   end
 end
