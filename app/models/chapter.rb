01c142e6210a6f8ee48c57cfcf7fe13565209266
Chavjoh <j.chavaillaz@gmail.com>
2014-04-13 23:29:02 +0200

    Add presence validation for chapter and tutorial.

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,6 @@ class Chapter < ActiveRecord::Base
   belongs_to :tutorial
   has_many :questions, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,6 +2,9 @@ class Tutorial < ActiveRecord::Base
   belongs_to :user
   has_many :chapters, dependent: :destroy
   
+  validates :title, presence: true
+  validates :difficulty, presence: true
+  
   def self.search(query)
     where("title like ?", "%#{query}%") 
   end
