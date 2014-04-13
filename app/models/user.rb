ce1354baecdb915035613bf51913e5cdc79735d0
Chavjoh <j.chavaillaz@gmail.com>
2014-04-13 23:12:37 +0200

    Add validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
